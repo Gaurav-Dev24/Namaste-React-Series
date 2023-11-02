fae80fd9b1329abf64bf902588e908604f09fdfa
Gaurav-Dev24 <subhamkr1995dob@gmail.com>
2023-11-02 13:08:09 +0530

    Day 1 code with separate App.js file

---
+++ Day-1/App.js
@@ -0,0 +1,10 @@
+// const heading = React.createElement("h1", {className: "title"}, "Namaste React");
+//       const paragraph = React.createElement("p",{className:"description"},"Hello this is paragraph");
+//     //   console.log(heading)
+//     //   console.log(paragraph)
+//     //   use correct dom function to call class or id
+//       const root = ReactDOM.createRoot(document.getElementById("root"));
+//     //   to render single element and it takes only one argument 
+//     //   root.render(heading);
+//     // to render multiple elements
+//       root.render([heading,paragraph]);
