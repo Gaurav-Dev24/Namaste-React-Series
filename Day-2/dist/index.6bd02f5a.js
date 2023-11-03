e25e56281880195fea607bc02922fb98ff34e105
Gaurav-Dev24 <subhamkr1995dob@gmail.com>
2023-11-03 16:09:14 +0530

    creating react app using parcel and creating each file manually

---
+++ Day-2/dist/index.6bd02f5a.js
@@ -0,0 +1,23 @@
+const heading = React.createElement("h1", {
+    className: "title"
+}, "Namaste React");
+const paragraph = React.createElement("p", {
+    className: "description"
+}, "Hello this is paragraph");
+const container = React.createElement("div", {
+    id: "container"
+}, [
+    heading,
+    paragraph
+]);
+// console.log(heading)
+// console.log(paragraph)
+//   use correct dom function to call class or id
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//   to render single element and it takes only one argument
+//   root.render(heading);
+// to render multiple elements we two ways
+// root.render([heading,paragraph]);
+root.render(container);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
