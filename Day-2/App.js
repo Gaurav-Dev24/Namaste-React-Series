1d3b8602352d59ae1f0b4ad05dd4848b5bd7744b
Gaurav-Dev24 <subhamkr1995dob@gmail.com>
2023-11-04 13:38:57 +0530

    creating react app using parcel and creating each file manually

--- Day-2/App.js
+++ Day-2/App.js
@@ -1,5 +1,5 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom/client"; // note always use "react-dom/client" to import ReactDOM
 
 const heading = React.createElement(
   "h1",
