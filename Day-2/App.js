228adcb0964ecd0514848484a458cc25a9ef9d11
Gaurav-Dev24 <subhamkr1995dob@gmail.com>
2023-11-08 13:55:00 +0530

    day 3 knowing the importance of having key for array elements in react

--- Day-2/App.js
+++ Day-2/App.js
@@ -3,18 +3,18 @@ import ReactDOM from "react-dom/client"; // note always use "react-dom/client" t
 
 const heading = React.createElement(
   "h1",
-  { className: "title" },
+  { className: "title", key: "h1" }, // specifying key for multiple child elements in container
   "Namaste React"
 );
 const paragraph = React.createElement(
   "p",
-  { className: "description" },
+  { className: "description", key: "p" }, // specifying key for multiple child elements in container
   "Hello this is paragraph"
 );
 const container = React.createElement("div", { id: "container" }, [
   heading,
   paragraph,
-]);
+]); // if we have multiple childs for an element we have to give them a unique key which can be anything
 // console.log(heading)
 // console.log(paragraph)
 //   use correct dom function to call class or id
