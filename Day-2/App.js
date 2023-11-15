5b8dd88bbdab240bff65a950a15d3a434a8d9679
Gaurav-Dev24 <subhamkr1995dob@gmail.com>
2023-11-15 12:11:56 +0530

    Understanding JSX and how it is executed

--- Day-2/App.js
+++ Day-2/App.js
@@ -12,12 +12,20 @@ const paragraph = React.createElement(
   { className: "description", key: "p" }, // specifying key for multiple child elements in container
   "Hello this is paragraph"
 );
+
+// JSX --> Creating elements using JSX an alternative to React.createElement
+const heading2 = (
+  <h2 id="title" key="h2">
+    Namaste Heading Two
+  </h2>
+); //--> JSX expression
+
 const container = React.createElement("div", { id: "container" }, [
   heading,
   paragraph,
+  heading2,
 ]); // if we have multiple childs for an element we have to give them a unique key which can be anything
-// console.log(heading)
-// console.log(paragraph)
+
 //   use correct dom function to call class or id
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //   to render single element and it takes only one argument
